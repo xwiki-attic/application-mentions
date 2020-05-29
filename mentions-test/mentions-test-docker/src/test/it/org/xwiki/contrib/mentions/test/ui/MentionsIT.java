5af9786b0d85214d039127d0f8fa4ab708284f32
Manuel Leduc <manuel.leduc@xwiki.com>
2020-05-29 10:55:28 +0200

    [misc] Docker based testing boilerplate.

---
+++ mentions-test/mentions-test-docker/src/test/it/org/xwiki/contrib/mentions/test/ui/MentionsIT.java
@@ -0,0 +1,140 @@
+package org.xwiki.contrib.mentions.test.ui;/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.xwiki.platform.notifications.test.po.NotificationsTrayPage;
+import org.xwiki.platform.notifications.test.po.NotificationsUserProfilePage;
+import org.xwiki.test.docker.junit5.TestReference;
+import org.xwiki.test.docker.junit5.UITest;
+import org.xwiki.test.ui.TestUtils;
+import org.xwiki.test.ui.po.ViewPage;
+import org.xwiki.test.ui.po.editor.WikiEditPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.xwiki.test.ui.po.BootstrapSwitch.State.ON;
+
+/**
+ * Test of the mentions application UI.
+ *
+ * @version $Id$
+ * @since 1.1
+ */
+@UITest
+public class MentionsIT
+{
+    public static final String U1_USERNAME = "U1";
+
+    public static final String USERS_PWD = "password";
+
+    public static final String U2_USERNAME = "U2";
+
+    public static final String APPLICATION_ID = "mentions.application.name";
+
+    public static final String ALTER_FORMAT = "alert";
+
+    /**
+     * A duplicate of {@link Runnable} wich allows to throw checked {@link Exception}.
+     * @see  Runnable
+     * @see <a href="https://www.baeldung.com/java-lambda-exceptions">Baeldung's Exceptions in Java 8 Lambda Expressions</a>.
+     */
+    @FunctionalInterface
+    private interface RunnableErr
+    {
+        void run() throws Exception;
+    }
+
+    /**
+     *
+     * <ul>
+     *     <li>Superadmin creates U1 and U2.</li>
+     *     <li>U2 activates the mentions notifications.</li>
+     *     <li>U1 adds a mention to U2.</li>
+     *     <li>U2 very that she has received a notification.</li>
+     * </ul>
+     *
+     * @param setup The test setup.
+     * @param reference The test page reference.
+     * @throws Exception In case of error.
+     */
+    @Test
+    @Order(1)
+    void basic(TestUtils setup, TestReference reference) throws Exception
+    {
+        actAsSuperAdmin(setup, () -> {
+            // create the users.
+            setup.createUser(U1_USERNAME, USERS_PWD, null);
+            setup.createUser(U2_USERNAME, USERS_PWD, null);
+        });
+
+        actAsUser(setup, U2_USERNAME, USERS_PWD, () -> {
+            // activate the notifications.
+            NotificationsUserProfilePage.gotoPage(U2_USERNAME)
+                .setApplicationState(APPLICATION_ID, ALTER_FORMAT, ON);
+        });
+
+        actAsUser(setup, U1_USERNAME, USERS_PWD, () -> {
+            // mention U2
+            ViewPage viewPage = setup.gotoPage(reference);
+            viewPage.waitUntilPageIsLoaded();
+            viewPage.edit();
+            WikiEditPage editPage = new WikiEditPage();
+            editPage.setContent("{{mention identifier=\"xwiki:XWiki.U2\" displayChoice=\"LOGIN\" /}}");
+            editPage.clickSaveAndView(true);
+        });
+
+        actAsUser(setup, U2_USERNAME, USERS_PWD, () -> {
+            setup.gotoPage("Main", "WebHome");
+            // check that a notif is well received
+            NotificationsTrayPage tray = new NotificationsTrayPage();
+            assertEquals(1, tray.getUnreadNotificationsCount());
+        });
+    }
+
+    /**
+     * Login as some user and perform some actions, then logout.
+     *
+     * @param setup The test setup.
+     * @param username The user's login.
+     * @param password The user's password.
+     * @param actions The actions to be performed.
+     * @throws Exception In case of errors.
+     */
+    private void actAsUser(TestUtils setup, String username, String password, RunnableErr actions) throws Exception
+    {
+        setup.login(username, password);
+        actions.run();
+        setup.gotoPage(setup.getURLToLogout());
+    }
+
+    /**
+     * Login as supermadmin, perform some actions, then logout.
+     *
+     * @param setup The test setup.
+     * @param actions Some actions.
+     * @throws Exception In case of error.
+     */
+    private void actAsSuperAdmin(TestUtils setup, RunnableErr actions) throws Exception
+    {
+        setup.loginAsSuperAdmin();
+        actions.run();
+        setup.gotoPage(setup.getURLToLogout());
+    }
+}
